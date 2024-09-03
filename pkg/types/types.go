786bb12109ebe8c77fff728c4aac14217ed8eccd
Yessentemir <yessentemir.utepov@gmail.com>
2024-09-03 10:39:15 +0500

    added types

---
+++ pkg/types/types.go
@@ -0,0 +1,34 @@
+package types
+
+// Money представляет собой денежную сумму в минимальных единицах (центы, копейки, дирамы и.т.д).
+type Money int64
+
+// PaymentCategory представляет собой категорию, в которой был совершен платеж (авто, аптеки, рестораны и.т.д.).
+type PaymentCategory string
+
+// PaymentStatus представляет собой статус платежа.
+type PaymentStatus string
+
+// Предопределенные статусы платежей.
+const (
+	PaymentStatusOk         PaymentStatus = "OK"
+	PaymentStatusFail       PaymentStatus = "FAIL"
+	PaymentStatusInProgress PaymentStatus = "INPROGRESS"
+)
+
+// Payment представляет информацию о платеже.
+type Payment struct {
+	ID       string
+	Amount   Money
+	Category PaymentCategory
+	Status   PaymentStatus
+}
+
+type Phone string
+
+// Account представляет информацию о счёте пользователя.
+type Account struct {
+	ID      int64
+	Phone   Phone
+	Balance Money
+}
