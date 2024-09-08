cb899c5c22555773c7741eb8fc1a9ef690626b66
Yessentemir <yessentemir.utepov@gmail.com>
2024-09-08 08:08:58 +0500

    added FindAccountByID and tests for it

--- pkg/wallet/service.go
+++ pkg/wallet/service.go
@@ -90,3 +90,17 @@ func (s *Service) Pay(accountID int64, amount types.Money, category types.Paymen
 	s.payments = append(s.payments, payment)
 	return payment, nil
 }
+
+func (s *Service) FindAccountByID(accountID int64) (*types.Account, error) {
+	var account *types.Account
+
+	for _, acc := range s.accounts {
+		if acc.ID == accountID {
+			account = acc
+			return account, nil
+		}
+	}
+
+	return nil, ErrAccountNotFound
+
+}
